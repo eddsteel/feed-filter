94d8ad47a798d6687820648e3ae925078704b170
Edd Steel <edward.steel@gmail.com>
2017-05-07 16:44:32 -0700

    Implement slice 3, better logging
    -- i.e. don't dump the whole extracted value, but show enough to be
       useful (also required use of Show and Eq). Yay!

--- src/main/scala/FeedFilters.scala
+++ src/main/scala/FeedFilters.scala
@@ -1,6 +1,8 @@
 package com.eddsteel.feedfilter
 
 import model._
+import cats.implicits._
+
 import java.net.URI
 
 object FeedFilters {
@@ -12,5 +14,12 @@ object FeedFilters {
         src = new URI("http://feeds.soundcloud.com/users/soundcloud:users:211911700/sounds.rss"),
         extract = FeedItemExtractor.title,
         rule = FilterNotRule(ContainsMatcher("""\bteaser\b""".r).test)
+      ),
+      FeedFilter(
+        name = "wittertainment",
+        src = new URI("http://www.bbc.co.uk/programmes/b00lvdrj/episodes/downloads.rss"),
+        extract = FeedItemExtractor.description,
+        rule = FilterNotRule(
+          ContainsMatcher("""\bsit(s|ting)? in for (mark|simon|kermode|mayo)\b| presents """.r).test)
       ))
 }
--- src/main/scala/model/FeedFilter.scala
+++ src/main/scala/model/FeedFilter.scala
@@ -1,12 +1,14 @@
 package com.eddsteel.feedfilter.model
 
 import Errors.FeedItemParseError
+import cats.implicits._
+import cats.Show
 
 import java.net.URI
 
 /** Represents a filter definition.
   */
-final case class FeedFilter[A](
+final case class FeedFilter[A: Show](
   name: String,
   src: URI,
   extract: FeedItemExtractor[A],
@@ -19,6 +21,6 @@ final case class FeedFilter[A](
       item <- FeedItem.fromXML(source)
       extracted = extract(item)
       result = rule.include(extracted)
-      _ = if (!result) println(s"SKIP $extracted")
+      _ = if (!result) println(s"SKIP ${extracted.show.take(100).takeWhile(_ =!= '\n')}")
     } yield result
 }
